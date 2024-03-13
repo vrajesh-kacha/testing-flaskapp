cd93f799f32c73af5e9df58f0e145e8ed7e0fe5e
vrajesh <kachavrajesh94@gmail.com>
2024-03-13 21:42:07 +0530

    update the code

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 CORS(app)
 @app.route("/")
 def home():
-    return "This is flask api"
+    return jsonify({"key":"This is flask api"})
 
 @app.route("/predict",methods=['POST'])
 def predict():
