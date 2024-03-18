41e308c7c9b456e71158d2a3aa2c13c2f0e4379a
vrajesh <kachavrajesh94@gmail.com>
2024-03-18 16:28:34 +0530

    flask testing code

--- app.py
+++ app.py
@@ -1,36 +1,50 @@
 from flask import Flask, request,jsonify
 import pickle
 import numpy as np
-from flask_cors import CORS
+
 app=Flask(__name__)
-CORS(app)
+# CORS(app)
 @app.route("/")
 def home():
     return jsonify({"key":"This is flask api"})
 
 @app.route("/predict",methods=['POST'])
 def predict():
-    Company=request.form.get("Company")
-    TypeName=request.form.get("TypeName")	
-    Inches=request.form.get("Inches")
-    Ram =request.form.get("Ram")	
-    Weight=request.form.get("Weight")
-    Touchscreen=request.form.get("Touchscreen")
-    ips=request.form.get("ips")
-    X_res=request.form.get("X_res")	
-    Y_res=request.form.get("Y_res")	
-    Cpu_brand=request.form.get("Cpu")	
-    HDD=request.form.get("HDD")
-    SSD=request.form.get("SSD")
-    Gpu_Brand=request.form.get("Gpu_Brand")	
-    Os=request.form.get("Os")
+        try:
+            
+            data=request.get_json()  # to get the data inform of json
+   
+#    to get the data in form of form-data
+            # Company=request.form.get("Company")
+            # TypeName=request.form.get("TypeName")	
+            # Inches=request.form.get("Inches")
+            # Ram =request.form.get("Ram")	
+            # Weight=request.form.get("Weight")
+            # Touchscreen=request.form.get("Touchscreen")
+            # ips=request.form.get("ips")
+            # X_res=request.form.get("X_res")	
+            # Y_res=request.form.get("Y_res")	
+            # Cpu_brand=request.form.get("Cpu")	
+            # HDD=request.form.get("HDD")
+            # SSD=request.form.get("SSD")
+            # Gpu_Brand=request.form.get("Gpu_Brand")	
+            # Os=request.form.get("Os")
 
-    input_query=np.array([[Company,TypeName,Inches,
-                           Ram,Weight,Touchscreen,
-                           ips,X_res,Y_res,Cpu_brand,
-                           HDD,SSD,Gpu_Brand,Os]])
-    
-    model=pickle.load(open("pipeline.pkl","rb"))
-    
-    return jsonify({"price": np.exp((model.predict(input_query)[0]))})
+            # input_query=np.array([[Company,TypeName,Inches,
+            #                Ram,Weight,Touchscreen,
+            #                ips,X_res,Y_res,Cpu_brand,
+            #                HDD,SSD,Gpu_Brand,Os]])
+            input_query=np.array([[data['Company'],data['TypeName'],
+                           float( data['Inches']),data['Ram'] ,float(data['Weight']),
+                            int(data['Touchscreen']),int(data['ips']),int(data['X_res']),
+                            int(data['Y_res']),data['Cpu_brand'],int(data['HDD']),
+                            int(data['SSD']),data['Gpu_Brand'],data['Os']
+                             ]])
+            model=pickle.load(open("pipeline.pkl","rb"))
+        except Exception as e:
+           print(e)
+        return jsonify({"price": np.exp((model.predict(input_query)[0]))})  
+    #   return jsonify(data)
 
+if __name__=='__main__':
+     app.run(debug=True)
